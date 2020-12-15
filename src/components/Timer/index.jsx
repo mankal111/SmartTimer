b09ae572b1f423f94ad20ff59ff7d1dfeefb2c62
mankal111 <mankal111@hotmail.com>
2020-12-15 18:07:37 +0200

    creation of timer component

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -1,8 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PlayToggle from '../PlayToggle';
+import TimeDisplay from '../TimeDisplay';
 import './Timer.css';
 
 function Timer() {
-    return <div class="container"></div>
+    const [remainingSeconds, setRemainingSeconds] = useState(3);
+    const [running, setRunning] = useState(false);
+    const [finished, setFinished] = useState(false);
+    const [intervalCb, setIntervalCb] = useState();
+    const [initialSeconds, setInitialSeconds] = useState(3);
+
+    const secondsToText = givenSeconds => {
+        const minutes = Math.floor(givenSeconds / 60);
+        const seconds = givenSeconds % 60;
+        return `${minutes}:${seconds<10 ? "0" : ""}${seconds}`;
+    }
+
+    const reduceTime = () => setRemainingSeconds(remainingSeconds => remainingSeconds - 1);
+
+    const startTimer = () => {
+        setIntervalCb(setInterval(reduceTime ,1000));
+        setRunning(true);
+    }
+
+    const stopTimer = () => {
+        clearInterval(intervalCb);
+        setRunning(false);
+    }
+
+    const resetTimer = () => {
+        setRemainingSeconds(initialSeconds);
+        setFinished(false);
+    }
+    
+    const timeText = secondsToText(remainingSeconds);
+    const [toggleIcon, toggleBtnCb] = running ?
+        ['pause', stopTimer] :
+        (
+            finished ?
+            ['reset', resetTimer] :
+            ['play', startTimer]
+        );
+
+    useEffect(() => {
+        document.title = `SmartTimer - ${timeText}`;
+        if (remainingSeconds === 0) {
+            stopTimer();
+            setFinished(true);
+        }
+    }, [remainingSeconds, timeText])
+
+    const setTimeButton = minutes => {
+        const clickHandler = () => {
+            stopTimer();
+            setRemainingSeconds(minutes * 60);
+            setInitialSeconds(minutes * 60);
+            setFinished(false);
+        };
+        return (
+            <button
+                className="setTimeButton"
+                onClick={clickHandler}
+            >
+                {`${minutes}m`}
+            </button>
+        );
+    } 
+    return (
+        <div className="container">
+            <PlayToggle icon={toggleIcon} onClick={toggleBtnCb} />
+            <TimeDisplay timeText={timeText} />
+            <div className="setTimeButtonsContainer">
+                {setTimeButton(.5)}
+                {setTimeButton(5)}
+                {setTimeButton(15)}
+                {setTimeButton(30)}
+            </div>
+        </div>
+    )
 }
 
 export default Timer;
